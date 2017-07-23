2f5effd8f8fbfb52bbbd6105d8811acff45e420e
JJ <jianan.beijing@gmail.com>
2017-07-22 22:12:19 -0700

    finish all instructions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,53 @@
-window.addEventListener('load', function() { 
 
-$(document).ready(function() {
-var green = Math.floor(Math.random() * 12 + 1);
-var white = Math.floor(Math.random() * 12 + 1);
-var pink = Math.floor(Math.random() * 12 + 1);
-var blue = Math.floor(Math.random() * 12 + 1);
-var score = 0;
-var losses = 0;
-var wins = 0;
+		var losses = 0;
+		var wins = 0;
+		var score = 0;
+		$(document).ready(function() {
+			randomGenerate();
+		});
 
-// generate the random number 
-var total = Math.floor(Math.random() * (120-19+1) + 19);
-		console.log( "it's" + total);
-	$("#total").html(total);
-	$(".green").attr("value",green);
-	$(".white").attr("value",white);
-	$(".pink").attr("value",pink);
-	$(".blue").attr("value",blue);
-
-// create "click" and add value to the score.
-	$("img").on("click", function(event){
-	var number = event.currentTarget.attributes[2].value;
-	console.log("this is " + number);
-	number = parseInt(number);
-	score += number;
-	console.log("hello", score);
-	$("textarea").html(score);
+		$("img").on("click", function(event){
+		var number = event.currentTarget.attributes[2].value;
+		number = parseInt(number); 
+		score += number;;
+		var total = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+		$("textarea").html(score);
 // set up wins and losses
-	if (total === score){
-		wins++;
-		$(".wins").html(wins);
+			console.log("total:" + total, "score: " + score)
+			if (total === score){
+				randomGenerate();
+				wins++;
+				$(".wins").html(wins); 
+			}
+			if(total < score){
+				randomGenerate();
+				losses++;
+				$(".losses").html(losses);
+			}
+
+			if(total > score){
+				console.log("KEEP GOING YOU ARE FINE.")
+			}
+
+		});
+
+	function randomGenerate (){
+		console.log("hiii")
+		var green = Math.floor(Math.random() * 12 + 1);
+		var white = Math.floor(Math.random() * 12 + 1);
+		var pink = Math.floor(Math.random() * 12 + 1);
+		var blue = Math.floor(Math.random() * 12 + 1);
+		score = 0;
+		$("textarea").empty();
+
+	// generate the random number 
+		var total = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+		console.log( "it's" + total);
+		$("#total").html(total);
+		$(".green").attr("value",green);
+		$(".white").attr("value",white);
+		$(".pink").attr("value",pink);
+		$(".blue").attr("value",blue);
+
 	}
 
-	else if(total < score){
-		losses++;
-		$(".losses").html(losses);
-	}
-
-	else{
-		if(losses!==0){
-			
-		}
-	}
-
-	});
-// function initializeGame(){
-// 	var total
-// 	var green = Math.floor(Math.random() * 12 + 1);
-// 	var white = Math.floor(Math.random() * 12 + 1);
-// 	var pink = Math.floor(Math.random() * 12 + 1);
-// 	var blue = Math.floor(Math.random() * 12 + 1);
-// 	var score = 0;
-// }
-		
-		
-
-	});
-
-});
